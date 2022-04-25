aac330fbb50f54fd7a88c1b8ccaf331172662666
Juma81 <jonathanumana10@gmail.com>
2022-04-24 18:10:49 -0600

    se agrego components/Number.js

--- components/Game.jsx
+++ components/Game.jsx
@@ -1,23 +1,48 @@
 import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Number from "./Number";
 
-export default Game = ( { randomNumbers } ) => {
+export default Game = ({ randomNumbersCount }) => {
+    const [randomNumbers, setRandomNumbers] = useState([]);
+    const [target, setTarget] = useState(0);
+    const [selectedNumbers, setSelectedNumbers] = useState([]);
+   
+
     // const target  = 10 + Math.floor(40 * Math.random()); //10....50//
-    const numbers = Array.from({ length: randomNumbers })
-    .map(() => 1 + Math.floor(10 * Math.random()));
+    // const numbers = Array.from({ length: randomNumbers }).map(() => 1 + Math.floor(10 * Math.random()));
+    // const target = numbers.slice(0, randomNumbers - 2).reduce((acc, cur) => acc + cur, 0);
 
-    const target = numbers.slice(0, randomNumbers -2)
-    .reduce( (acc, cur) => acc + cur, 0);
+    //No Array → Exc all the time
+    //Empty array → Exc once the first time
+    //Full array → Exec on Change
+    //Return → Exc on dismount
+    useEffect(() => {
+        const numbers = Array.from({ length: randomNumbersCount }).map(() => 1 + Math.floor(10 * Math.random()));
+        const target = numbers.slice(0, randomNumbersCount - 2).reduce((acc, cur) => acc + cur, 0);
 
+        setRandomNumbers(numbers);
+        setTarget(target);
+    }, []);
+
+    const isNumberSelected = (numberIndex) =>
+        selectedNumbers.some((number) => number === numberIndex);
+    const selectNumber = (number) =>
+        setSelectedNumbers([...selectedNumbers, number]);
 
     return (
         <View>
-            <Text style ={styles.target} >{target}</Text>
-            <View style ={styles.randomContainer} >
-            {numbers.map((number, index) => (
-                <Text key={index}style={styles.random}>{number}</Text>
-            ) )}
-        </View>
+            <Text style={styles.target}>{target}</Text>
+            <View style={styles.randomContainer}>
+                {randomNumbers.map((number, index) => (
+                    <Number
+                        key={index}
+                        id={index}
+                        number={number}
+                        isSelected={isNumberSelected(index)}
+                        onSelected={selectNumber}
+                    />
+                ))}
+            </View>
         </View>
     );
 };
@@ -25,21 +50,13 @@ export default Game = ( { randomNumbers } ) => {
 const styles = StyleSheet.create({
     target: {
         fontSize: 40,
-        backgroundColor: '#aaa',
-        textAlign: 'center',
+        backgroundColor: "#aaa",
+        textAlign: "center",
     },
     randomContainer: {
         flex: 1,
-        flexDirection: 'row',
-        flexWrap: 'wrap',
-        justifyContent: 'space-between',
+        flexDirection: "row",
+        flexWrap: "wrap",
+        justifyContent: "space-between",
     },
-    random: {
-        backgroundColor: '#999',
-        width: 100,
-        marginHorizontal: 15,
-        marginVertical: 25,
-        fontSize: 35,
-        textAlign:'center'
-    }
 });
